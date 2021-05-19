a12b19a219cd67bfae28150ecf160dab16ce78f4
damien <damien.savoldelli@gmail.com>
2021-05-19 22:01:02 +0200

    fix(fastify): fix forgotten async key in exercice 2 done

--- 1-fastify/2-exercice-done.js
+++ 1-fastify/2-exercice-done.js
@@ -8,7 +8,7 @@ const server = fastify()
 const db = await open({ filename: ':memory:', driver: sqlite3.Database })
 await db.run(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT NOT NULL UNIQUE)`)
 
-server.get('/', (_, response) => {
+server.get('/', async (_, response) => {
   // Afficher dans la console les utilisateurs
   const results = await db.get('SELECT * FROM users')
   console.log(results)
@@ -20,7 +20,7 @@ server.get('/about', (_, response) => {
   response.type('text/html').send('<h1>Abous Us</h1>')
 })
 
-server.post('/', (request, response) => {
+server.post('/', async (request, response) => {
   console.log(request.body)
 
   // Stocker un nouvel utilisateur avec son email
