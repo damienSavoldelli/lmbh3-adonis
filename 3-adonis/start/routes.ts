efec7b979a3fd4f3fa462199aee2997af8b5a634
damien <damien.savoldelli@gmail.com>
2021-05-25 08:47:41 +0200

    feat(adonis): create user form routes (GET,POST)

--- 3-adonis/start/routes.ts
+++ 3-adonis/start/routes.ts
@@ -19,7 +19,14 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
+})
+
+Route.on('/users').render('form')
+
+Route.post('/user', async ({ request }) => {
+  User.create(request.all())
 })
